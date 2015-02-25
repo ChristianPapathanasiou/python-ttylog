c677131fe54f130fa08e8cb11f3f3a743b95ec38
Christian Papathanasiou <cp@gcx.io>
2015-02-25 11:02:20 -0500

    first commit

---
+++ ttylog.py
@@ -0,0 +1,20 @@
+from subprocess import Popen,PIPE
+import re
+import sys
+
+pipe = Popen(['strace','-p',sys.argv[1],'-e','read'],shell=False, stdout=PIPE, stderr=PIPE)
+tmp_string = ""
+
+while True:
+        output = pipe.stderr.readline()
+        if (output.find("\\r\\n") >= 0):
+                print tmp_string
+                tmp_string = ""
+        else:
+                pattern = r'"([a-z]|[A-Z]|\\r\\n|-| |@|\.\.)"'
+                m = re.search(pattern,output)
+                if (m is None):
+                        continue
+                else:
+                        tmp_string = tmp_string + m.group()[:-1][-1:]
+
